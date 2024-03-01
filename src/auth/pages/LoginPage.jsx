cd62c5b5f69ed3048c22017b49c7e136c5df4bf0
Jhonatan Zapata <jhonatanzapatarol@gmail.com>
2024-03-01 11:44:37 -0500

    Commit inicial

---
+++ src/auth/pages/LoginPage.jsx
@@ -0,0 +1,31 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context';
+
+export const LoginPage = () => {
+  const navigate = useNavigate();
+  const { login } = useContext( AuthContext );
+
+  const onLogin = () => {
+    login('Jhonatan Zapata');
+
+    navigate('/', {
+      replace: true
+    });
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button 
+        className="btn btn-primary"
+        onClick={ onLogin }
+      >
+        Ingresar
+      </button>
+
+    </div>
+  )
+}
---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import { useContext } from "react"
+
+import { AuthContext } from "../auth"
+
+export const PrivateRoute = ( { children } ) => {
+    const { logged } = useContext( AuthContext );
+    return ( logged )
+        ? children
+        : <Navigate to="/login" />
+}
